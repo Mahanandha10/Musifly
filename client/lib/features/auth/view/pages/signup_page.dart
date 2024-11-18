06fb33ac8f7672ec781f50870f101d2211df0bdf
Mahanandha10 <mahanandhan2002@gmail.com>
2024-11-18 09:23:56 +0530

    signup page

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -14,47 +14,62 @@ class _SignupPageState extends State<SignupPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       // const TextStyle('Musifly'),
+       title: Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Text(
+      'Welcome Musifly',
+      style: TextStyle(
+        fontSize: 40,
       ),
+    ),
+  ),
+  centerTitle: true,
+),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
               'Sign Up',
-            style: TextStyle(
-              fontSize: 50,
-          fontWeight: FontWeight.bold,
-          
-        ),
-        ),
-        const SizedBox(height: 30),
-        CustomField(
-          hintText: 'Username',
-        ), 
-        const SizedBox(height: 15),
-        CustomField(
-          hintText: 'Password',
-        ),
-        const SizedBox(height: 15), 
-        CustomField(
-          hintText: 'Email',
-        ), 
-        const SizedBox(height: 20),  
-        AuthGradientButton(),
-        const SizedBox(height: 20), 
-        //  RichText(
-        //   text:TextSpan(
-        //     text: 'Already have an account?',
-        //     children: [
-        //       TextSpan(text: 'Sign In',
-        //       ),
-        //     ],
-        //   ),   
-        //   ), 
-        ],
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            const CustomField(
+              hintText: 'Username',
+            ),
+            const SizedBox(height: 15),
+            const CustomField(
+              hintText: 'Password',
+            ),
+            const SizedBox(height: 15),
+            const CustomField(
+              hintText: 'Email',
+            ),
+            const SizedBox(height: 20),
+            const AuthGradientButton(),
+            const SizedBox(height: 20),
+            RichText(
+              text: const TextSpan(
+                text: 'Already have an account? ',
+                style: TextStyle(
+                    color: Colors.white), // Set the default color to white
+                children: [
+                  TextSpan(
+                    text: 'Sign In',
+                    style: TextStyle(
+                      color: Colors
+                          .blue, // Optional: Set a different color for "Sign In"
+                      fontWeight: FontWeight.bold, // Optional: Make it bold
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
