5a55f9eaac5462bf64d38966bfb5dd63eddc8401
Mahanandha10 <mahanandhan2002@gmail.com>
2025-01-12 19:29:10 +0530

    MP3 PLAYER

---
+++ client/lib/splash_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate to the main screen after a delay
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/main'); // Replace '/home' with your main route
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black, // Background color
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/logo.png',
+              width: 200, // Adjust the width as needed
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Musifly', // Replace with your app name or tagline
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
